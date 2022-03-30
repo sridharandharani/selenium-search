e67d5a7d59526bb63573086e3b0e60ec71b2a54f
sridharandharani <sridharandharani1815@gmail.com>
2022-03-30 10:26:13 +0530

    google search

---
+++ test_selinium.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+print("Testcase started ")
+
+driver.maximize_window()
+driver.get("https://www.google.com/")
+driver.find_element_by_name("q").send_keys("HARMAN")
+time.sleep(2)
+driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
+time.sleep(10)
+driver.close()
+print("testcase completed")
+
+
